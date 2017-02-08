2082074411615309f4eaf9c5ae5e4a29e09dfd4f
chalkley_zhou <chalkley_zhou@ehsy.com>
2017-02-08 10:24:28 +0800

    md

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var store = require('../service/store');
 
-router.get('/', async function (ctx, next) {
+router.get('/getRoute', async function (ctx, next) {
     var rows = await store.getRouteList();
     var route = JSON.parse(JSON.stringify(rows))[0];
     console.log(route.lines);
     route.lines = route.lines.split(',');
-    ctx.body = JSON.stringify(route);
+    ctx.body = JSON.stringify(route)
 });
 
 module.exports = router;
